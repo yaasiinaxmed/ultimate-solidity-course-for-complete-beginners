981eeb596ea46112d8e8c31bb844b229d4ed31fd
yaasiinaxmed <yaskassoy@gmail.com>
2024-10-25 13:18:23 +0300

    Done Twitter Struct Exercise

--- Exercises/13-Twitter-Struct-Exercise.sol
+++ Exercises/13-Twitter-Struct-Exercise.sol
@@ -2,27 +2,39 @@
 
 pragma solidity ^0.8.0;
 
-// 1️⃣ Define a Tweet Struct with author, content, timestamp, likes
-// 2️⃣ Add the struct to array
-// 3️⃣ Test Tweets
+// 1️⃣ Define a Tweet Struct with author, content, timestamp, likes: Done
+// 2️⃣ Add the struct to array: Done
+// 3️⃣ Test Tweets: Done
 
 contract Twitter {
 
     // define our struct
-
-
-    // add our code
-    mapping(address => string[] ) public tweets;
-
-    function createTweet(string memory _tweet) public {
-        tweets[msg.sender].push(_tweet);
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    function getTweet(address _owner, uint _i) public view returns (string memory) {
+    // add our code
+    mapping(address => Tweet[] ) public tweets;
+
+    function createTweet(string memory _tweet) public {
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(address _owner, uint _i) public view returns (Tweet memory) {
         return tweets[_owner][_i];
     }
 
-    function getAllTweets(address _owner) public view returns (string[] memory ){
+    function getAllTweets(address _owner) public view returns (Tweet[] memory ){
         return tweets[_owner];
     }
 
