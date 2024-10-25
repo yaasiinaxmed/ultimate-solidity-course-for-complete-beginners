c098adaa6fd821b41cf04bf61e0c9d7741da4b43
yaasiinaxmed <yaskassoy@gmail.com>
2024-10-25 16:18:06 +0300

    Done Modifiers exercise

--- Exercises/16-Modifiers-Exercise.sol
+++ Exercises/16-Modifiers-Exercise.sol
@@ -14,12 +14,12 @@ contract PausableToken {
     }
 
     modifier onlyOwner() {
-        // 1️⃣ Implement the modifier to allow only the owner to call the function
+        // 1️⃣ Implement the modifier to allow only the owner to call the function: Done
         require(msg.sender == owner, "You Are Not The Owner!");
         _;
     }
 
-    // 2️⃣ Implement the modifier to check if the contract is not paused
+    // 2️⃣ Implement the modifier to check if the contract is not paused: Done
     modifier notPaused() {
         require(paused == false , "The contract is Paoused!");
         _;
@@ -33,7 +33,7 @@ contract PausableToken {
         paused = false;
     }
 
-    // 3️⃣ use the notPaused modifier in this function 
+    // 3️⃣ use the notPaused modifier in this function: Done
     function transfer(address to, uint amount) public notPaused {
         require(balances[msg.sender] >= amount, "Insufficient balance");
 
